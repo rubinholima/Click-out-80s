616a6601f541a0c15236c7f0883d243c9fcb5a52
rubinholima <rubinholima@me.com>
2020-02-19 14:42:14 -0500

    Updates

---
+++ precache-manifest.1564ea12019bb5091e556c0aed1eb46b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e07f927ca7709855d34c6d1c5e498f3",
+    "url": "/Click-out-80s/index.html"
+  },
+  {
+    "revision": "cff1b2979044c0a5f6d9",
+    "url": "/Click-out-80s/static/css/main.da03f0fb.chunk.css"
+  },
+  {
+    "revision": "11b58d6810a98059c253",
+    "url": "/Click-out-80s/static/js/2.8b3cd8b0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Click-out-80s/static/js/2.8b3cd8b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff1b2979044c0a5f6d9",
+    "url": "/Click-out-80s/static/js/main.8c7eb2f2.chunk.js"
+  },
+  {
+    "revision": "283619b8e68865e2170b",
+    "url": "/Click-out-80s/static/js/runtime-main.c9050aaf.js"
+  },
+  {
+    "revision": "72d372c3cbccd1de2c9a6bc4305fb546",
+    "url": "/Click-out-80s/static/media/winner.72d372c3.jpg"
+  }
+]);
